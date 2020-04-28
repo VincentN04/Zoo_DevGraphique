3b52d418b8cafa68a056330eab28f9566b2779dc
Vincent Nadeau <Vincentn007@hotmail.ca>
2020-04-28 17:35:17 -0400

    fix mouvement joueur

---
+++ TP2 DevGraphique/TP2 DevGraphique/Heros.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    class Heros
+    {
+        public static int X = 12 * 32;
+        public static int Y = 7 * 32;
+
+        public class HerosPos
+        {
+            public int X
+            {
+                get;
+                set;
+            }
+
+            public int Y
+            {
+                get;
+                set;
+            }
+        }
+        
+    }
+
+   
+}
--- TP2 DevGraphique/TP2 DevGraphique/ZooInterface.cs
+++ TP2 DevGraphique/TP2 DevGraphique/ZooInterface.cs
@@ -117,6 +117,18 @@ namespace TP2_DevGraphique
             return EstHeros;
         }
 
-       
+        /// <summary>
+        /// Détecte le click des touches de flèches
+        /// </summary>
+        /// <param name="msg"></param>
+        /// <param name="keyData"></param>
+        /// <returns> La touche pesée </returns>
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            carteJeu1.EvenementClick(keyData);
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+
     }
 }
