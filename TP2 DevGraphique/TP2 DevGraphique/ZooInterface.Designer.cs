ceed372e468a1bc585b9da4e22db7c3b98449f3f
Vincent Nadeau <Vincentn007@hotmail.ca>
2020-05-14 10:40:27 -0400

    Publication du changement de nom et de taille de fenêtre

--- TP2 DevGraphique/TP2 DevGraphique/ZooInterface.Designer.cs
+++ TP2 DevGraphique/TP2 DevGraphique/ZooInterface.Designer.cs
@@ -30,41 +30,38 @@ namespace TP2_DevGraphique
         /// </summary>
         private void InitializeComponent()
         {
-
-       
-            this.carteJeu1 = new TP2.CarteJeu();
             this.toolStripInformative1 = new TP2_DevGraphique.ToolStripInformative();
+            this.carteJeu1 = new TP2.CarteJeu();
             this.SuspendLayout();
-            
-            // 
-            // carteJeu1
-            // 
-            this.carteJeu1.Location = new System.Drawing.Point(-4, 26);
-            this.carteJeu1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
-            this.carteJeu1.Name = "carteJeu1";
-            this.carteJeu1.Size = new System.Drawing.Size(1276, 827);
-            this.carteJeu1.TabIndex = 0;
-            this.carteJeu1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.carteJeu1_MouseDown);
             // 
             // toolStripInformative1
             // 
             this.toolStripInformative1.BackColor = System.Drawing.Color.DarkOliveGreen;
             this.toolStripInformative1.Location = new System.Drawing.Point(-3, -1);
+            this.toolStripInformative1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.toolStripInformative1.Name = "toolStripInformative1";
-            this.toolStripInformative1.Size = new System.Drawing.Size(962, 28);
+            this.toolStripInformative1.Size = new System.Drawing.Size(1275, 28);
             this.toolStripInformative1.TabIndex = 1;
+            // 
+            // carteJeu1
+            // 
+            this.carteJeu1.Location = new System.Drawing.Point(-4, 26);
+            this.carteJeu1.Margin = new System.Windows.Forms.Padding(5);
+            this.carteJeu1.Name = "carteJeu1";
+            this.carteJeu1.Size = new System.Drawing.Size(1276, 827);
+            this.carteJeu1.TabIndex = 0;
+            this.carteJeu1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.carteJeu1_MouseDown);
             // 
             // ZooInterface
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(954, 690);
+            this.ClientSize = new System.Drawing.Size(1269, 849);
             this.Controls.Add(this.toolStripInformative1);
-
             this.Controls.Add(this.carteJeu1);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "ZooInterface";
-            this.Text = "Form1";
+            this.Text = "Le Zoo Rigolo";
             this.ResumeLayout(false);
 
         }
